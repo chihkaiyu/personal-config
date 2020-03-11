693a9955ee7029f158fc826ae300e334beeae9a6
Kai Yu <kaiyu@17.media>
2020-03-11 16:36:32 +0800

    :wrench:

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ex
 
-# install zsh
-sudo apt-get install -y zsh
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install zsh and change default shell
+brew install zsh
+chsh -s /bin/zsh
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -21,3 +25,19 @@ git config --global alias.st status
 # change git config
 git config --global user.name "Chih Kai Yu"
 git config --global user.email "kai.chihkaiyu@gmail.com"
+
+# python venvs
+mkdir -p ~/.local/bin ~/.local/venvs ~/.local/pythons/2.7 ~/.local/pythons/3.7
+python3 -m venv ~/.local/venvs/pipenv
+~/.local/venvs/pipenv/bin/pip install pipenv
+ln -s ~/.local/venvs/pipenv/bin/pipenv ~/.local/bin
+pipenv --version
+
+git clone git://github.com/pyenv/pyenv.git ~/Documents/pyenv
+sh ~/Documents/pyenv/plugins/python-build/install.sh
+
+python-build 2.7.17 ~/.local/pythons/2.7
+ln -s ~/.local/pythons/2.7/python2.7 ~/.local/bin
+
+python-build 3.7.7 ~/.local/pythons/3.7
+ln -s ~/.local/pythons/3.7/python3.7 ~/.local/bin
